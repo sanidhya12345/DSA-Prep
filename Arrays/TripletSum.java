c420930a06896a1b740abf8d2eab7b07578e6738
sanidhya12345 <{sanidhya.varshney_cs19@gla.ac.in}+{sanidhya12345}@users.noreply.github.com>
2024-09-23 17:22:01 +0530

    triplet sum

---
+++ Arrays/TripletSum.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class TripletSum {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5};
+        int tripletSum=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                for(int k=j+1;k<arr.length;k++){
+                    tripletSum+=arr[i]+arr[j]+arr[k];
+                }
+            }
+        }
+        System.out.println(tripletSum);
+    }
+}
