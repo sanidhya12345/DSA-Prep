09bd8892b8d10fa990cda7ceef199c22e55093ab
sanidhya12345 <{sanidhya.varshney_cs19@gla.ac.in}+{sanidhya12345}@users.noreply.github.com>
2024-09-25 09:31:58 +0530

    divisible sum pairs hackerrank

---
+++ HackerrankBeginner/DivisibleSumPairs.java
@@ -0,0 +1,18 @@
+package HackerrankBeginner;
+
+import java.util.List;
+
+public class DivisibleSumPairs {
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+    // Write your code here
+       int count=0;
+       for(int i=0;i<n;i++){
+           for(int j=i+1;j<n;j++){
+               if((ar.get(i)+ar.get(j))%k==0){
+                   count++;
+               }
+           }
+       }
+       return count;
+    }
+}
