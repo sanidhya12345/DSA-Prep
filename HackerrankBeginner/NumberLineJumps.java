12e1d45a47947ed0252e0d1846d282c78bd27a1b
sanidhya12345 <{sanidhya.varshney_cs19@gla.ac.in}+{sanidhya12345}@users.noreply.github.com>
2024-09-25 09:23:27 +0530

    Number Line Jumps

---
+++ HackerrankBeginner/NumberLineJumps.java
@@ -0,0 +1,13 @@
+package HackerrankBeginner;
+public class NumberLineJumps {
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        if((x2>x1 && v2>=v1) || (x1>x2 && v1>=v2)){
+            return "NO";
+        }
+        else if((x2-x1)%(v1-v2)==0){
+            return "YES";
+        }
+        return "NO";
+
+    }
+}
