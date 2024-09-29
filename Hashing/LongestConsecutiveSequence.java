3591452989bc2a6a9785b049652a89a413011060
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-09-29 12:09:33 +0530

    lcs medium leetcode

---
+++ Hashing/LongestConsecutiveSequence.java
@@ -0,0 +1,27 @@
+package Hashing;
+import java.util.*;
+
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : nums) {
+            if (!numSet.contains(num - 1)) {
+                int length = 1;
+
+                while (numSet.contains(num + length)) {
+                    length++;
+                }
+
+                longest = Math.max(longest, length);
+            }
+        }
+
+        return longest;  
+    }
+}
