05474b422b609dfbb427c9537494924b0b08b405
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-10-10 11:48:19 +0530

    4 sum ii leetcode

---
+++ Hashing/FourSumII.java
@@ -0,0 +1,21 @@
+package Hashing;
+import java.util.*;
+public class FourSumII {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:nums1){
+            for(int j:nums2){
+                map.put(i+j,map.getOrDefault(i+j,0)+1);
+            }
+        }
+        int count=0;
+        for(int i:nums3){
+            for(int j:nums4){
+                if(map.containsKey(-(i+j))){
+                    count+=map.get(-(i+j));
+                }
+            }
+        }
+        return count;
+    }
+}
