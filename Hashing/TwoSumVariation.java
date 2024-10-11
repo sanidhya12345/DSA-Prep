8f0a80dbcc37205668fd431b7378eb9d2854f526
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-10-11 17:21:25 +0530

    two sum variation problem solution

---
+++ Hashing/TwoSumVariation.java
@@ -0,0 +1,24 @@
+package Hashing;
+import java.util.*;
+public class TwoSumVariation {
+
+    public static boolean twoSumVariation(int [] a,int target){
+        HashSet<Integer> set=new HashSet<>();
+        for(int num:a){
+            if(set.contains(target-num)){
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int target=sc.nextInt();
+        int [] a=new int[n];
+        for(int i=0;i<n;i++) a[i]=sc.nextInt();
+        System.out.println(twoSumVariation(a, target));
+        sc.close();
+    }
+}
