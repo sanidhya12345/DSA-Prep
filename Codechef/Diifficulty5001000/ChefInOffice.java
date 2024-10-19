a0b7319335c480a52bcf0c28d7e1e735bc1fe685
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-10-19 11:07:26 +0530

    codechef 500-1000

---
+++ Codechef/Diifficulty5001000/ChefInOffice.java
@@ -0,0 +1,15 @@
+package Codechef.Diifficulty5001000;
+
+import java.util.*;
+public class ChefInOffice {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int test=sc.nextInt();
+        while (test--!=0) {
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            System.out.println(4*x+y);
+        }
+        sc.close();
+    }
+}
