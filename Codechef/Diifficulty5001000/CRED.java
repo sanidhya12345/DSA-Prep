c390f95213c6405e894281265879fd02a7a7e2d0
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-10-19 11:18:12 +0530

    CRED

---
+++ Codechef/Diifficulty5001000/CRED.java
@@ -0,0 +1,14 @@
+package Codechef.Diifficulty5001000;
+import java.util.*;
+public class CRED {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int test=sc.nextInt();
+        while (test--!=0) {
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            System.out.println((x*y)/100);
+        }
+        sc.close();
+    }
+}
