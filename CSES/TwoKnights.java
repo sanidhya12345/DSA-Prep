e8af8f0a6ff7e55b34dbf3e15d7e9dba471e2f5a
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-10-24 18:06:48 +0530

    two knights

---
+++ CSES/TwoKnights.java
@@ -0,0 +1,26 @@
+package CSES;
+
+import java.util.Scanner;
+
+public class TwoKnights {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int k = 1; k <= n; k++) {
+            // Total ways to place two knights
+            long totalPositions = (long) k * k * (k * k - 1) / 2;
+            
+            // Attacking pairs
+            long attackingPositions = 4 * (k - 1) * (k - 2);
+            
+            // Non-attacking pairs
+            long nonAttacking = totalPositions - attackingPositions;
+            
+            // Output result for k x k board
+            System.out.println(nonAttacking);
+        }
+        
+        sc.close();
+    }
+}
