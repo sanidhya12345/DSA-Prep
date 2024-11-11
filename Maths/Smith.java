70a8b0390a47536ea6003f53facc3dc03d8b4daf
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-11-11 17:39:35 +0530

    update

--- Maths/Smith.java
+++ Maths/Smith.java
@@ -1,7 +1,6 @@
 package Maths;
 
 import java.io.*;
-import java.util.StringTokenizer;
 public class Smith {
     static final StdIn in = new StdIn();
     static final PrintWriter out=new PrintWriter(System.out);
@@ -38,62 +37,3 @@ public class Smith {
         out.flush();
     }
 }
-
-
-
-class StdIn
-    {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public StdIn()
-        {
-            br = new BufferedReader(new
-                     InputStreamReader(System.in));
-        }
-
-        String next()
-        {
-            while (st == null || !st.hasMoreElements())
-            {
-                try
-                {
-                    st = new StringTokenizer(br.readLine());
-                }
-                catch (IOException  e)
-                {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt()
-        {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong()
-        {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble()
-        {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine()
-        {
-            String str = "";
-            try
-            {
-                str = br.readLine();
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
