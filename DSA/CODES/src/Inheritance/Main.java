672dc43a2c3b0a98076408ac6a560981f16b4384
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-11-12 10:29:53 +0530

    inheritance example

---
+++ DSA/CODES/src/Inheritance/Main.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Main extends Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main m=new Main();
+		System.out.println(m.add(1, 2));
+	}
+
+}
