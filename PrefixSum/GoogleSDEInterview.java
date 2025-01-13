ca376e360ca8e426a3654607c69964de94e95946
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2025-01-13 21:35:47 +0530

    find the longest increasing subarray after change atmost 1 element

--- PrefixSum/GoogleSDEInterview.java
+++ PrefixSum/GoogleSDEInterview.java
@@ -43,8 +43,8 @@ public class GoogleSDEInterview {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    int[] arr = new int[n];
-    for (int i = 0; i < n; i++) {
+    int[] arr = new int[n + 1];
+    for (int i = 1; i <= n; i++) {
       arr[i] = sc.nextInt();
     }
     System.out.println(longestIncreasing(n, arr));
