499951bf42018cbf495b9173f569f0f561a2f3b5
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2025-01-18 16:57:15 +0530

    false commit

---
+++ Arrays/HelloArray.java
@@ -0,0 +1,5 @@
+public class HelloArray {
+    public static void main(String[] args) {
+        System.out.println("Hello this is the array which we are talking about");
+    }
+}
--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("hello this is sanidhya");
-    }
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
 }
